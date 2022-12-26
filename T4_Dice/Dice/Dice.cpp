9c2074f2f5ba4e499ebb0a34e9a7102051135bb1
Chikita <chipot2121@gmail.com>
2022-12-26 13:31:11 +0300

    fix dice

--- T4_Dice/Dice/Dice.cpp
+++ T4_Dice/Dice/Dice.cpp
@@ -9,30 +9,30 @@ int main()
 	char cs[] = "1d20,1d6+2,5d8+4";
 	std::cout << Dice::throwsDices(cs) << std::endl;
 
-	/*const int range = 20;
+	//const int range = 18;
 
-	char c[] = "1d20";
+	//char c[] = "3d6";
 
-	Dice dice = Dice(c);
+	//Dice dice = Dice(c);
 
-	int a[range];
+	//int a[range];
 
 
-	for (int i = 0; i < range; i++)
-	{
-		a[i] = 0;
-	}
+	//for (int i = 0; i < range; i++)
+	//{
+	//	a[i] = 0;
+	//}
 
-	for (int i = 0; i < 1000; i++)
-	{
-		a[dice.throwDice() - 1]++;
-	}
+	//for (int i = 0; i < 1000; i++)
+	//{
+	//	a[dice.throwDice() - 1]++;
+	//}
 
-	for (int i = 0; i < range; i++)
-	{
-		std::cout << i + 1 << "|" << a[i] << std::endl;
-	}*/
+	//for (int i = 0; i < range; i++)
+	//{
+	//	std::cout << i + 1 << "|" << a[i] << std::endl;
+	//}
 
-	return 1;
+	//return 1;
 }
 
